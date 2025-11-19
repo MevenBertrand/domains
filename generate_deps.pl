3911c70864a3f427d9a63a2c606949309577fbcc
Meven <mgapb2@cam.ac.uk>
2025-11-19 19:38:57 +0100

    infra

--- generate_deps.pl
+++ generate_deps.pl
@@ -1,11 +1,16 @@
 print "digraph logrel_deps {\n";
 # See here for color schemes: https://graphviz.org/doc/info/colors.html
 print "  node [shape = ellipse,style=filled,colorscheme = paired12];\n";
+print "  subgraph cluster_categories { label=\"categories\" \n}";
 while (<>) {
   if (m/.*?theories\/([^\s]*)\.vo.*:(.*)/) {
     $dests = $2 ;
     ($path,$src) = ($1 =~ s/\//\./rg =~ m/(.*\.)?([^.]*)$/);
-    print "\"$path$src\"[label=\"$src\"]" ;
+    if ($path =~ m/categories\./) {
+      print "subgraph cluster_categories { \"$path$src\"[label=\"$src\",fillcolor=1]}"
+    }else {
+      print "\"$path$src\"[label=\"$src\",fillcolor=6,fontcolor=white]"
+    }
     for my $dest (split(" ", $dests)) {
       $dest =~ s/\//\./g ;
       print "  \"$1\" -> \"$path$src\";\n" if ($dest =~ m/theories\.(.*)\.vo/);
